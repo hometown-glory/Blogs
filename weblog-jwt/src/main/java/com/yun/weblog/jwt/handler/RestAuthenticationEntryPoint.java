fd0dce3efa12a06891eb6c1682d61b31f441c45a
admin <1783736194@qq.com>
2024-10-29 10:29:39 +0800

    完成了后端工程模块的整合Spring Security 整合 JWT ：实现接口鉴定权限

---
+++ weblog-jwt/src/main/java/com/yun/weblog/jwt/handler/RestAuthenticationEntryPoint.java
@@ -0,0 +1,36 @@
+package com.yun.weblog.jwt.handler;
+
+import com.yun.weblog.common.enums.ResponseCodeEnum;
+import com.yun.weblog.common.result.Response;
+import com.yun.weblog.jwt.utils.ResultUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author 云言月
+ * 2024/10/28 22:43
+ * description: 用户未登录访问受保护的资源
+ */
+@Slf4j
+@Component
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        log.warn("用户未登录访问受保护的资源: ", authException);
+        if (authException instanceof InsufficientAuthenticationException) {
+            ResultUtil.fail(response, HttpStatus.UNAUTHORIZED.value(), Response.fail(ResponseCodeEnum.UNAUTHORIZED));
+        }
+
+        ResultUtil.fail(response, HttpStatus.UNAUTHORIZED.value(), Response.fail(authException.getMessage()));
+    }
+
+}
